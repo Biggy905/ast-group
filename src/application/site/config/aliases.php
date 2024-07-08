06ceb3d46ff7606bbc9f1557fa8778c768dbcf25
biggy <alexander-a-bolgov@yandex.ru>
2024-07-08 18:57:35 +0300

    Добавлена конфигурация для сайта.

---
+++ src/application/site/config/aliases.php
@@ -0,0 +1,9 @@
+<?php
+
+$rootDir = dirname(dirname(dirname(__DIR__)));
+$applicationDir = dirname(dirname(__DIR__));
+
+Yii::setAlias('web', $applicationDir . '/site/public');
+Yii::setAlias('webroot', $applicationDir . '/site/public');
+
+Yii::setAlias('resoursesMain', $applicationDir . '/site/resourses/main');
