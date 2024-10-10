742d20f211b4310c6c3030a74f9c1d2953a6daf7
biggy <alexander-a-bolgov@yandex.ru>
2024-10-10 18:45:30 +0300

    Подготовка логгера

--- src/application/admin/config/containers.php
+++ src/application/admin/config/containers.php
@@ -1,5 +1,12 @@
 <?php
 
 return [
+    \Psr\Log\LoggerInterface::class => static function () {
+        $processor = new \Monolog\Processor\WebProcessor();
 
+        $logger = new \Monolog\Logger('common');
+        $logger->pushProcessor($processor);
+
+        return $logger;
+    }
 ];
